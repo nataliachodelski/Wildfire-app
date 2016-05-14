9e579073df1e4d67c8482fe6089895507c1926bf
Natalia <nchodelski@gmail.com>
2016-05-14 11:17:24 -0700

    added fb stuff to the app delegate

--- Hackathon App/AppDelegate.swift
+++ Hackathon App/AppDelegate.swift
@@ -7,8 +7,8 @@
 //
 
 import UIKit
-#import <FBSDKCoreKit/FBSDKCoreKit.h>
-
+import FBSDKCoreKit
+import FBS
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -18,7 +18,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        return FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
         return true
+    }
+    
+    func application(application: UIApplication,
+                     openURL url: NSURL,
+                             sourceApplication: String?,
+                             annotation: AnyObject?) -> Bool {
+        return FBSDKApplicationDelegate.sharedInstance().application(
+            application,
+            openURL: url,
+            sourceApplication: sourceApplication,
+            annotation: annotation)
     }
 
     func applicationWillResignActive(application: UIApplication) {
@@ -37,6 +50,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationDidBecomeActive(application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        FBSDKAppEvents.activateApp()
     }
 
     func applicationWillTerminate(application: UIApplication) {
---
+++ Hackathon App/Objective-CBridgingHeader.m
@@ -0,0 +1,11 @@
+//
+//  Objective-CBridgingHeader.m
+//  Hackathon App
+//
+//  Created by Natalia on 14/05/2016.
+//  Copyright © 2016 NataliaDeveloper. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+
