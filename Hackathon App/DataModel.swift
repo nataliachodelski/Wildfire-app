c3eb7058d0afcbff797d3520b986cafb4159781a
Natalia <nchodelski@gmail.com>
2016-06-06 23:58:49 -0700

    about to add detail view and link up. right now storyboard represents old still image for detail view

--- Hackathon App/DataModel.swift
+++ Hackathon App/DataModel.swift
@@ -12,20 +12,134 @@ import SwiftyJSON
 
 class DataModel : NSObject {
     
-    //var demoData = NSData(contentsOfURL: NSURL(fileURLWithPath: "projects.json"))
+    class var sharedInstance: DataModel {
+        struct Singleton {
+            static let instance = DataModel()
+        }
+        return Singleton.instance
+    }
     
     var currentEvents : [ProjectObject] = []
+    var smallImages : [UIImage] = []
+    var introImages : [UIImage] = []
+    var fullImages  : [UIImage] = []
     
+    var eventCount : Int
+    var currentEvent : Int
+    
+    
+    //var demoData = NSData(contentsOfURL: NSURL(fileURLWithPath: "projects.json"))
+
+    var jsonFileName : String
+    
+    override init() {
+        self.jsonFileName = "projects"
+        eventCount = 0
+        currentEvent = -1
+        
+        super.init()
+        
+        let smallImg1 = UIImage(named: "demoJapan")
+        let smallImg2 = UIImage(named: "demoEcuador")
+        let smallImg3 = UIImage(named: "demoSyria")
+        
+        let fullImg1 = UIImage(named: "fullJapan")
+        let fullImg2 = UIImage(named: "fullEcuador")
+        let fullImg3 = UIImage(named: "fullSyria")
+
+        let introImg1 = UIImage(named: "Intro1")
+        let introImg2 = UIImage(named: "Intro2")
+        let introImg3 = UIImage(named: "Intro3")
+        let introImg4 = UIImage(named: "Intro4")
+        
+        smallImages = [smallImg1!, smallImg2!, smallImg3!]
+        fullImages = [fullImg1!, fullImg2!, fullImg3!]
+
+        introImages = [introImg1!, introImg2!, introImg3!, introImg4!]
+        
+        parseJSON(jsonFileName)
+    }
+    
+    
+    func getProjectObject(index: Int) -> ProjectObject? {
+        //if currentEvent != -1 {
+         return currentEvents[index]
+//        } else {
+//            print("no current event, ie is -1")
+//            return nil
+//        }
+    }
+ 
+    func parseJSON(fileName: String) {
+        print("running parseJSON)")
+        
+        if let path = NSBundle.mainBundle().pathForResource(fileName, ofType: "json")
+        {
+            do {
+                let data = try NSData(contentsOfURL: NSURL(fileURLWithPath: path), options: NSDataReadingOptions.DataReadingMappedIfSafe)
+                let json = JSON(data: data)
+                if json != JSON.null {
+                    print("[Data model] got jsonData in SwiftyJson")
+                } else {
+                    print("could not get json from file, make sure that file contains valid json.")
+                }
+                
+                // iterate through the root nodes
+                for key in json
+                {
+                    eventCount += 1
+                    print("[Outer] object key is \(key.0)")
+                    
+                    let projectObj = ProjectObject()
+                    
+                    // iterate through each root object and extract needed info via keys in the subjson
+                    for innerKey in key.1
+                    {
+                        switch (innerKey.0) {
+                        case "org":
+                            projectObj.orgName = innerKey.1.stringValue
+                            
+                        case "location":
+                            projectObj.eventLocation = innerKey.1.stringValue
+                            
+                        case "event":
+                            projectObj.eventType = innerKey.1.stringValue
+                            
+                        case "detail":
+                            projectObj.eventDescription = innerKey.1.stringValue
+                            
+                        case "impact":
+                            projectObj.eventImpact = innerKey.1.stringValue
+                            
+                        default:
+                            break;
+                        }
+                    }
+                    
+                    projectObj.smallImage = smallImages[eventCount - 1]
+                    projectObj.fullImage = fullImages[eventCount - 1]
+                    
+                    self.currentEvents.append(projectObj)
+                }
+            }
+            catch let error as NSError {
+                print(error.localizedDescription)
+            }
+            
+        } else {
+            print("[Data model] While parsing Json - found Invalid filename/path")
+        }
+    }
 }
 
-
-
+// object that stores info for each project/cause listd
 class ProjectObject : NSObject {
     internal var orgName : String!
     internal var eventType : String!
     var eventLocation : String!
     var eventDescription : String!
     var eventImpact : String!// optionall dictonary with associated GPS coordinates or something
-    var image : UIImage!
-
+    var smallImage : UIImage!
+    var fullImage : UIImage!
+    
 }
--- ProjectListTableViewController.swift
+++ ProjectListTableViewController.swift
@@ -11,86 +11,16 @@ import SwiftyJSON
 
 class ProjectListTableViewController: UITableViewController {
     
-    let dataModel = DataModel()
-    
-    var jsonFileName : String = "projects"
-    var eventCount : Int = 0
-    
-    var demoImages : [UIImage] = []
-    let demoImg1 = UIImage(named: "demoEcuador")
-    let demoImg2 = UIImage(named: "demoJapan")
-    let demoImg3 = UIImage(named: "demoSyria")
-    
+    let dataModel = DataModel.sharedInstance
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.tableView.rowHeight = 160
         
-        demoImages = [demoImg1!, demoImg2!, demoImg3!]
-        
-        parseJSON(jsonFileName)
     }
     
-    func parseJSON(fileName: String) {
-        print("running parseJSON)")
-        
-        if let path = NSBundle.mainBundle().pathForResource(fileName, ofType: "json")
-        {
-            do {
-                let data = try NSData(contentsOfURL: NSURL(fileURLWithPath: path), options: NSDataReadingOptions.DataReadingMappedIfSafe)
-                json = JSON(data: data)
-                if json != JSON.null {
-                    print("got jsonData in SwiftyJson")
-                } else {
-                    print("could not get json from file, make sure that file contains valid json.")
-                }
-            }
-            catch let error as NSError {
-                print(error.localizedDescription)
-            }
-            
-        } else {
-            print("Invalid filename/path.")
-        }
-        
-        // iterate through the root nodes
-        for key in json
-        {
-            eventCount += 1
-            print("[Outer] object key is \(key.0)")
-            
-            let projectObj = ProjectObject()
-            
-            // iterate through each root object and extract needed info via keys in the subjson
-            for innerKey in key.1
-            {
-                switch (innerKey.0) {
-                case "org":
-                    projectObj.orgName = innerKey.1.stringValue
-                    
-                case "location":
-                    projectObj.eventLocation = innerKey.1.stringValue
-                    
-                case "event":
-                    projectObj.eventType = innerKey.1.stringValue
-                    
-                case "detail":
-                    projectObj.eventDescription = innerKey.1.stringValue
-                    
-                case "impact":
-                    projectObj.eventImpact = innerKey.1.stringValue
-                    
-                default:
-                    break;
-                }
-            }
-            
-            projectObj.image = demoImages[eventCount - 1]
-            
-            dataModel.currentEvents.append(projectObj)
-        }
-    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -102,7 +32,7 @@ class ProjectListTableViewController: UITableViewController {
     }
     
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return eventCount
+        return dataModel.eventCount
     }
     
     
@@ -120,6 +50,11 @@ class ProjectListTableViewController: UITableViewController {
         return cell
     }
     
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        let row = indexPath.row
+        dataModel.currentEvent = row
+    }
     
     /*
      // Override to support rearranging the table view.
